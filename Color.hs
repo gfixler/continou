3e75ece8ccf8c880907b86647c03387cd94d453d
gfixler <gary@garyfixler.com>
2016-04-12 09:04:49 -0700

    Add fgnum :: Color -> Int

--- Color.hs
+++ Color.hs
@@ -16,3 +16,21 @@ data Color = Black
            | White
            deriving (Bounded,Enum,Eq,Show)
 
+fgnum :: Color -> Int
+fgnum Black     = 30
+fgnum Red       = 31
+fgnum Green     = 32
+fgnum Yellow    = 33
+fgnum Blue      = 34
+fgnum Magenta   = 35
+fgnum Cyan      = 36
+fgnum LtGray    = 37
+fgnum DkGray    = 90
+fgnum LtRed     = 91
+fgnum LtGreen   = 92
+fgnum LtYellow  = 93
+fgnum LtBlue    = 94
+fgnum LtMagenta = 95
+fgnum LtCyan    = 96
+fgnum White     = 97
+
