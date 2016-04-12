d0677b451639f0256a3c860e560724f41996c1cf
gfixler <gary@garyfixler.com>
2016-04-12 10:34:59 -0700

    Add initial render function

--- Image.hs
+++ Image.hs
@@ -14,3 +14,6 @@ lerp :: (Enum a, Fractional a) => a -> a -> Int -> [a]
 lerp s e n = [(e - s) / n' * i + s | i <- [0..n']]
     where n' = fromIntegral (n - 1)
 
+render :: (Double,Double) -> (Double,Double) -> Int -> Int -> Image a -> [a]
+render (l,b) (r,t) w h f = [f (x,y) | y <- lerp t b h, x <- lerp l r w]
+
