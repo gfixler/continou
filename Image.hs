82de8a65fa0dc1390cc7f8f26bcb722be07a601d
gfixler <gary@garyfixler.com>
2016-04-12 10:13:27 -0700

    Add lerp :: (...) => a -> a -> Int -> [a]

--- Image.hs
+++ Image.hs
@@ -10,3 +10,7 @@ always = const
 leftAndRight :: a -> a -> Image a
 leftAndRight l r = \(x,y) -> if x < 0 then l else r
 
+lerp :: (Enum a, Fractional a) => a -> a -> Int -> [a]
+lerp s e n = [(e - s) / n' * i + s | i <- [0..n']]
+    where n' = fromIntegral (n - 1)
+
