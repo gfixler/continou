cc9dae8dcee0ae119caf9c41bd836c451c6c953a
gfixler <gary@garyfixler.com>
2016-04-12 08:55:07 -0700

    Add Color data type

---
+++ Color.hs
@@ -0,0 +1,18 @@
+data Color = Black
+           | Red
+           | Green
+           | Yellow
+           | Blue
+           | Magenta
+           | Cyan
+           | LtGray
+           | DkGray
+           | LtRed
+           | LtGreen
+           | LtYellow
+           | LtBlue
+           | LtMagenta
+           | LtCyan
+           | White
+           deriving (Bounded,Enum,Eq,Show)
+
