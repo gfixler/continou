87c6b2fa33f924b986f5f117b82362254736a7a3
gfixler <gary@garyfixler.com>
2016-04-12 10:36:36 -0700

    Break up render output from [a] to [[a]] rows

--- Image.hs
+++ Image.hs
@@ -14,6 +14,7 @@ lerp :: (Enum a, Fractional a) => a -> a -> Int -> [a]
 lerp s e n = [(e - s) / n' * i + s | i <- [0..n']]
     where n' = fromIntegral (n - 1)
 
-render :: (Double,Double) -> (Double,Double) -> Int -> Int -> Image a -> [a]
-render (l,b) (r,t) w h f = [f (x,y) | y <- lerp t b h, x <- lerp l r w]
+render :: (Double,Double) -> (Double,Double) -> Int -> Int -> Image a -> [[a]]
+render (l,b) (r,t) w h f =
+        [[f (x,y) | x <- lerp l r w] | y <- lerp t b h]
 
