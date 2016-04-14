d64fbd1d6e3945ffd0063ba8d1b1d89ea43ffa0f
gfixler <gary@garyfixler.com>
2016-04-14 11:54:57 -0700

    Add keyToCont, keysToCont functions

--- KeyFSM.hs
+++ KeyFSM.hs
@@ -1,2 +1,14 @@
 module KeyFSM where
 
+keyToCont :: Char -> IO ()
+keyToCont c = putStrLn msg >> f
+    where msg = "Press " ++ show c ++ " to continue..."
+          f = do c' <- getChar
+                 if c' == c then return () else f
+
+keysToCont :: [Char] -> IO ()
+keysToCont cs = putStrLn msg >> f
+    where msg = "Press one of " ++ show cs ++ " to continue..."
+          f = do c' <- getChar
+                 if c' `elem` cs then return () else f
+
