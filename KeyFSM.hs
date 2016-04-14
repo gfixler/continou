ac9a46c6e671e508dbebb0bbbcb996b0a507d0b2
gfixler <gary@garyfixler.com>
2016-04-14 12:10:17 -0700

    Import System.IO buffering, echo functions

--- KeyFSM.hs
+++ KeyFSM.hs
@@ -1,5 +1,11 @@
 module KeyFSM where
 
+import System.IO ( BufferMode(NoBuffering)
+                 , hSetBuffering, hSetEcho
+                 , hGetBuffering, hGetEcho
+                 , stdin, stdout, getChar
+                 )
+
 trap :: (Char -> Bool) -> String -> IO Char
 trap p s = putStrLn s >> f
     where f = do c' <- getChar
