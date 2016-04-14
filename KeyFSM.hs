ae06aaf20045616c3d085235a0a48e4e127ed136
gfixler <gary@garyfixler.com>
2016-04-14 11:36:06 -0700

    Add silently :: IO a -> IO a

--- KeyFSM.hs
+++ KeyFSM.hs
@@ -6,6 +6,24 @@ import System.IO ( BufferMode(NoBuffering)
                  , stdin, stdout, getChar
                  )
 
+silently :: IO a -> IO a
+silently f = do
+    inB <- hGetBuffering stdin
+    outB <- hGetBuffering stdout
+    inE <- hGetEcho stdin
+    outE <- hGetEcho stdout
+    hSetBuffering stdin NoBuffering
+    hSetBuffering stdout NoBuffering
+    hSetEcho stdout True -- HACK: required for clean exit
+    hSetEcho stdin False
+    hSetEcho stdout False
+    r <- f
+    hSetBuffering stdin inB
+    hSetBuffering stdout outB
+    hSetEcho stdin inE
+    hSetEcho stdout outE
+    return r
+
 trap :: (Char -> Bool) -> String -> IO Char
 trap p s = putStrLn s >> f
     where f = do c' <- getChar
