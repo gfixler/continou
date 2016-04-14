2503c302d27664448bb19a4707614cf3d890b5b5
gfixler <gary@garyfixler.com>
2016-04-14 10:07:03 -0700

    Add Explorer type, Show instance, Color default

--- Interact.hs
+++ Interact.hs
@@ -3,3 +3,16 @@ module Interact where
 import Color
 import Image
 
+data Explorer a = Explorer { img :: Image a
+                           , lb  :: Coord
+                           , rt  :: Coord
+                           , w   :: Int
+                           , h   :: Int
+                           }
+
+instance Show a => Show (Explorer a) where
+    show e = show (render (img e) (grid (lb e) (rt e) (w e) (h e)))
+
+defColorExplorer :: Explorer Color
+defColorExplorer = Explorer (const White) (-5,-5) (5,5) 21 10
+
