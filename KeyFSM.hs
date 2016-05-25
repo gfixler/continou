e04fd43e207524338846e98f68bcf12149fe75cb
gfixler <gary@garyfixler.com>
2016-05-25 08:03:29 -0700

    Add allow to KeyFSM.hs

--- KeyFSM.hs
+++ KeyFSM.hs
@@ -29,3 +29,7 @@ trap p s = putStrLn s >> f
     where f = do c' <- getChar
                  if p c' then return c' else f
 
+allow :: (Char -> Bool) -> IO Char
+allow p = do c <- getChar
+             if p c then (putChar c >> return c) else allow p
+
